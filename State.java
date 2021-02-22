3b0e00028f466d6ff49e66efff10ca6a47b89891
rwhilton13 <65377041+rwhilton13@users.noreply.github.com>
2021-02-22 02:24:55 -0500

    Update State.java

--- State.java
+++ State.java
@@ -1,25 +1,27 @@
 
+import java.util.ArrayList;
 public class State {
 	public Space[][] board;
-	private int boardWidth;
-	private int minimax;
-	public State parent;
-	//public State leftChild;
-	//public State rightSibling;
-	private boolean terminal;
+	public int boardWidth;
+	public int minimax;
+	//public State parent;
+	public State minMaxedChild;
+	public boolean terminal;
+	public ArrayList<State> childStates;
 	
-	public State(Space[][] board, State parent, int minimax, boolean terminal) {
+	public State(Space[][] board, int minimax) {
 		this.board = board;
-		this.parent = parent;
+		//this.parent = parent;
 		this.minimax = minimax;
-		this.terminal = terminal;
+		this.terminal = false;
 		boardWidth = board.length;
+		childStates = new ArrayList<State>();
 	}
 
-	public boolean isTerminal() { return terminal; }
-	public void setTerminal(boolean terminal) { this.terminal = terminal; }
-	public int getMinimax() { return minimax; }
-	public void setMinimax(int minimax) { this.minimax = minimax; }
+//	public boolean isTerminal() { return terminal; }
+//	public void setTerminal(boolean terminal) { this.terminal = terminal; }
+//	public int getMinimax() { return minimax; }
+//	public void setMinimax(int minimax) { this.minimax = minimax; }
 
 	public void printState() {
 		//dark/black = x , light/white = o
@@ -50,5 +52,193 @@ public class State {
 		}
 		System.out.println();
 		
+	}
+	
+	//flipPieces returns a State
+	
+	public String opponentCharacter(String given) {
+		if(given.equals("X")) {
+			return "O";
+		}
+		return "X";
+	}
+
+	public boolean checkValidity(int row, int col, String given) {
+		return (checkRight(row, col, given) || checkLeft(row, col, given) 
+				|| checkUp(row, col, given) || checkDown(row, col, given) 
+				|| checkDiagTR(row, col, given) || checkDiagTL(row, col, given)
+				|| checkDiagBR(row, col, given) || checkDiagBL(row, col, given));
+	}	
+
+	private boolean checkRight(int row, int col, String given) {
+		String opp = opponentCharacter(given);
+		boolean seenOpp = false;
+		int here = col;
+		for(int i = col + 1; i < boardWidth; i++) {
+			if(board[row][i].equals(opp)) {
+				seenOpp = true;
+			}
+			if(board[row][i].equals(given)) {
+				here = i;
+				break;
+			}
+		}
+		if(here != col && seenOpp && board[row][here].equals(given)) {
+			System.out.println("Right: VALID MOVE AT: (" + row + "," + here + ")");
+			return true;
+		}
+		return false;
+	}
+
+	private boolean checkLeft(int row, int col, String given) {
+		String opp = opponentCharacter(given);
+		boolean seenOpp = false;
+		int here = col;
+		for(int i = col - 1; i >= 0; i--) {
+			if(board[row][i].equals(opp)) {
+				seenOpp = true;
+			}
+			if(board[row][i].equals(given)) {
+				here = i;
+				break;
+			}
+		}
+		if(here != col && seenOpp && board[row][here].equals(given)) {
+			System.out.println("Left: VALID MOVE AT: (" + row + "," + here + ")");
+			return true;
+		}
+		return false;
+	}
+
+	private boolean checkUp(int row, int col, String given) {
+		String opp = opponentCharacter(given);
+		boolean seenOpp = false;
+		int here = row;
+		for(int i = row - 1; i >= 0; i--) {
+			if(board[i][col].equals(opp)) {
+				seenOpp = true;
+			}
+			if(board[i][col].equals(given)) {
+				here = i;
+				break;
+			}
+		}
+		if(here != row && seenOpp && board[here][col].equals(given)) {
+			System.out.println("UP: VALID MOVE AT: (" + here + "," + col + ")");
+			return true;
+		}
+		return false;
+	}
+
+	private boolean checkDown(int row, int col, String given) {
+		String opp = opponentCharacter(given);
+		boolean seenOpp = false;
+		int here = row;
+		for(int i = row + 1; i < boardWidth; i++) {
+			if(board[i][col].equals(opp)) {
+				seenOpp = true;
+			}
+			if(board[i][col].equals(given)) {
+				here = i;
+				break;
+			}
+		}
+		if(here != row && seenOpp && board[here][col].equals(given)) {
+			System.out.println("Down: VALID MOVE AT: (" + here + "," + col + ")");
+			return true;
+		}
+		return false;
+	}
+
+	private boolean checkDiagTR(int row, int col, String given) {
+		String opp = opponentCharacter(given);
+		boolean seenOpp = false;
+		int tempRow = row - 1;
+		int tempCol = col + 1;
+
+		while(tempRow >= 0 && tempCol < boardWidth) {
+			if(board[tempRow][tempCol].equals(opp)) {
+				seenOpp = true;
+			}
+			if(board[tempRow][tempCol].equals(given)) {
+				break;
+			}
+			tempRow--;
+			tempCol++;
+		}
+		if(tempRow != row && tempCol != col && seenOpp && board[tempRow][tempCol].equals(given)) {
+			System.out.println("Down: VALID MOVE AT: (" + tempRow + "," + tempCol + ")");
+			return true;
+		}
+		return false;
+	}
+
+	private boolean checkDiagTL(int row, int col, String given) {
+		String opp = opponentCharacter(given);
+		boolean seenOpp = false;
+		int tempRow = row - 1;
+		int tempCol = col - 1;
+
+		while(tempRow >= 0 && tempCol >= 0) {
+			if(board[tempRow][tempCol].equals(opp)) {
+				seenOpp = true;
+			}
+			if(board[tempRow][tempCol].equals(given)) {
+				break;
+			}
+			tempRow--;
+			tempCol--;
+		}
+		if(tempRow != row && tempCol != col && seenOpp && board[tempRow][tempCol].equals(given)) {
+			System.out.println("Down: VALID MOVE AT: (" + tempRow + "," + tempCol + ")");
+			return true;
+		}
+		return false;
+	}
+	
+	private boolean checkDiagBR(int row, int col, String given) {
+		String opp = opponentCharacter(given);
+		boolean seenOpp = false;
+		int tempRow = row + 1;
+		int tempCol = col + 1;
+
+		while(tempRow < boardWidth && tempCol < boardWidth) {
+			if(board[tempRow][tempCol].equals(opp)) {
+				seenOpp = true;
+			}
+			if(board[tempRow][tempCol].equals(given)) {
+				break;
+			}
+			tempRow++;
+			tempCol++;
+		}
+		if(tempRow != row && tempCol != col && seenOpp && board[tempRow][tempCol].equals(given)) {
+			System.out.println("Down: VALID MOVE AT: (" + tempRow + "," + tempCol + ")");
+			return true;
+		}
+		return false;
+	}
+
+	private boolean checkDiagBL(int row, int col, String given) {
+		String opp = opponentCharacter(given);
+		boolean seenOpp = false;
+		int tempRow = row + 1;
+		int tempCol = col - 1;
+		
+		while(tempRow < boardWidth && tempCol >= 0) {
+			if(board[tempRow][tempCol].equals(opp)) {
+				seenOpp = true;
+			}
+			if(board[tempRow][tempCol].equals(given)) {
+				break;
+			}
+			tempRow++;
+			tempCol--;
+		}
+		if(tempRow != row && tempCol != col && seenOpp && board[tempRow][tempCol].equals(given)) {
+			System.out.println("Down: VALID MOVE AT: (" + tempRow + "," + tempCol + ")");
+			return true;
+		}
+		return false;
 	}
 }
