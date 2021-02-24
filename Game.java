48f5ced698738f4bd5734911b443f378a2f5c620
rwhilton13 <65377041+rwhilton13@users.noreply.github.com>
2021-02-23 21:17:37 -0500

    Update Game.java

--- Game.java
+++ Game.java
@@ -1,19 +1,21 @@
 import java.util.ArrayList;
 public class Game {
-	public String player, AI; //x vs o color
+	public char player, AI; //x vs o color
 	public int boardWidth;
-	public State currentState, childState;
+	public State currentState;
+	public State childState;
 	public int tempMinimax;
 	
-	public Game(String player, String AI, int boardWidth) {
+	public Game(char player, char AI, int boardWidth) {
 		this.player = player;
 		this.AI = AI;
 		this.boardWidth = boardWidth;
 		currentState = initialState(boardWidth, boardWidth);
+		
 	}
 	
-	public void updateBoardPlayer(int row, int col, String color) {
-		currentState.board[row][col].setColor(color);
+	public void updateBoardPlayer(int row, int col, char color) {
+		currentState.board[row][col] = color;
 		currentState = currentState.flipPieces(row,col,color); //looks for any sandwich in direction and flips them
 		currentState.printState();
 	}
@@ -24,6 +26,7 @@ public class Game {
 		} else {
 			currentState = minimaxDecision(currentState);
 		}
+		System.out.println("program go brrr");
 		currentState.printState();
 	}
 	public State findBestChild(State currentState) {
@@ -32,6 +35,8 @@ public class Game {
 	//a function that will be given a state and return its minMaxedChild for next move
 
 	public State minimaxDecision(State s) {
+		System.out.println("CurrentState: ");
+		s.printState();
 		s.childStates = getChildStatesMax(s);
 		for(State child: s.childStates) {
 			if(minValue(child).minimax > s.minimax) {
@@ -102,24 +107,50 @@ public class Game {
 		
 		for(int r = 0; r < boardWidth; r++) {
 			for(int c = 0; c < boardWidth; c++) {
-				if(s.checkValidity(r,c,AI)) {
-					childState = currentState;
-					childState.board[r][c].setColor(AI);
-					childState = childState.flipPieces(r,c,AI);
+				if(s.checkValidity(r,c,player)) {
+					childState = s;
+					//childState = copyState(s);
+					childState.board[r][c] = player;
+					childState = childState.flipPieces(r,c,player);
 					s.childStates.add(childState);
 				}
 			}
 		}
 		return s.childStates;
 	}
-	public ArrayList<State> getChildStatesMax(State s){
-		
+	
+	public State copyBoard(State s) {
+		State temp = new State(new char[boardWidth][boardWidth], -100000);
 		for(int r = 0; r < boardWidth; r++) {
 			for(int c = 0; c < boardWidth; c++) {
-				if(s.checkValidity(r,c,player)) {
-					childState = currentState;
-					childState.board[r][c].setColor(player);
-					childState = childState.flipPieces(r,c,player);
+				temp.board[r][c] = s.board[r][c];
+			}
+		}
+		
+		return temp;
+	}
+	
+	public ArrayList<State> getChildStatesMax(State s) {
+		//System.out.println("haha brrr ");
+		for(int r = 0; r < boardWidth; r++) {
+			for(int c = 0; c < boardWidth; c++) {
+				System.out.println("Testing "+c+":"+r+" with "+AI);
+				if(s.checkValidity(r,c,AI)) {
+					System.out.println("CurrentState: ");
+					s.printState();
+					//these getChildStates methods are applying a move to the next child instead of the state s
+					childState = copyBoard(s); //problematic statement is probably childState = s
+					//childState = copyState(s);
+					
+					childState.board[r][c] = AI;
+					//this line is first place where we see error. Most likely because childState is an alias of s.
+					//to remedy this entire problem, you can probably make a copyState method that takes the board and its other parameters.
+					
+					childState = childState.flipPieces(r,c,AI);
+					
+					System.out.println("CurrentState after applications: ");
+					s.printState();
+					
 					s.childStates.add(childState);
 				}
 			}
@@ -135,15 +166,15 @@ public class Game {
 		int xs = 0, os = 0;
 		for(int r = 0; r < s.boardWidth; r++) {
 			for(int c = 0; c < s.boardWidth; c++) {
-				if(s.board[r][c].getColor().compareTo("x")==0) {
+				if(s.board[r][c] == 'x') {
 					xs++;
 				}
-				if(s.board[r][c].getColor().compareTo("o")==0) {
+				if(s.board[r][c] == 'o') {
 					os++;
 				}
 			}
 		}
-		if(player.compareTo("x") == 0) { //player=x,AI=o
+		if(player == 'x') { //player=x,AI=o
 			return os-xs; //minimax value
 		} else { //player=o,AI=x
 			return xs-os; //minimax value
@@ -152,7 +183,7 @@ public class Game {
 	
 	public State initialState(int numRows, int numCols){
 
-		Space[][] board = new Space[numRows][numCols];
+		char[][] board = new char[numRows][numCols];
 		char[] letters = {'a','b','c','d','e','f','g','h'};
 
 		for(int r = 0; r < numRows; r++) {
@@ -160,15 +191,15 @@ public class Game {
 				if((r == board.length/2 && c == board.length/2) || 
 						(r == board.length/2 - 1 && c == board.length/2 - 1)) {
 
-					board[r][c] = new Space(letters[c],r+1,"o");
+					board[r][c] = 'o';
 
 				} else if((r == board.length/2 - 1 && c == board.length/2) || 
 						(r == board.length/2 && c == board.length/2 - 1)) {
 
-					board[r][c] = new Space(letters[c],r+1,"x");
+					board[r][c] = 'x';
 
 				} else {
-					board[r][c] = new Space(letters[c],r+1,"empty");
+					board[r][c] = 'e';
 				}
 			}
 		}
