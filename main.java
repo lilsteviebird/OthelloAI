6b34bbf49f02d5a4974b8a7716f1922a7550367e
rwhilton13 <65377041+rwhilton13@users.noreply.github.com>
2021-02-24 18:55:09 -0500

    Update main.java

--- main.java
+++ main.java
@@ -1,75 +1,86 @@
 import java.util.Scanner;
 public class main {
 	static char player, AI;
+	static String input;
+	static int row,col;
+	static boolean playerTurn;
+	static Scanner scanner = new Scanner(System.in); 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Scanner scanner = new Scanner(System.in); 
+		
 		  
         // String input 
-        String input = null;
-        int row, col;
-		player = 'x';
-		AI = 'o';
-		boolean playerTurn = true;
+		player = 'e';
+		AI = 'e';
 		
+		System.out.println("Choose your Game:");
+		System.out.println("1. Small 4x4 Reversi");
+		System.out.println("2. Standard 8x8 Reversi");
+		Game g1 = null;
+		int gameType = scanner.nextInt();
+		System.out.println("Dark is always first to move.");
+		System.out.println("Do you want to play DARK (x) or LIGHT (o)?");
+		char turn = scanner.next().charAt(0);
 		
-//		System.out.println("Choose your Game:");
-//		System.out.println("1. Small 4x4 Reversi");
-//		System.out.println("2. Standard 8x8 Reversi");
-//		Game g1 = null;
-//		int gameType = scanner.nextInt();
-//		if(gameType == 1) {
-//			g1 = new Game(player, AI, 4);
-//		} else if(gameType == 2) {
-//			g1 = new Game(player, AI, 8);
-//		}
-		Game g1 = new Game(player, AI, 4);
+		if(turn == 'x') {
+			player = 'x';
+			AI = 'o';
+			playerTurn = true;
+		} else if(turn == 'o') {
+			player = 'o';
+			player = 'x';
+			playerTurn = false;
+		}
 		
+		if(gameType == 1) {
+			g1 = createGame(player, AI, 4);
+		} else if(gameType == 2) {
+			g1 = createGame(player, AI, 8);
+		}
+		runGame(g1);
+		
+	}
+	
+	public static Game createGame(char player, char AI, int boardWidth) {
+		return new Game(player, AI, boardWidth);
+	}
+	
+	public static void runGame(Game game) {
 		while(true) {
-			//have a if statement that determines if no more moves are possible
-			//if currentState terminalAI and terminalPlayer are both true
-			if(g1.currentState.terminalAI && g1.currentState.terminalPlayer) {
-				break;
-			}
 			if(playerTurn) {
 				System.out.println("What would you like your next move to be: ");
 				input = scanner.next();
 				col = letterToNumber(input.charAt(0));
 				row = Character.getNumericValue(input.charAt(1))-1;
 				
-				playerMove(g1, row, col);
+				playerMove(game, row, col);
 				playerTurn = false;
-				System.out.println("AI's Turn");
-				System.out.println("");
+				System.out.println("Now it's the AI's Turn to Move!");
 				System.out.println("");
 			} else {
-				AIMove(g1);
+				AIMove(game);
 				playerTurn = true;
-				System.out.println("Your Turn");
+				System.out.println("Now it's Your Turn to Move!");
 			}
 		}
-		
-		//for loop until game is complete
-		// alternate between player turn and AI turn
-		
 	}
+	
 	public static void playerMove(Game g, int moveRow, int moveCol) {
 		if(g.currentState.checkValidity(moveRow, moveCol, player)) {
 			g.updateBoardPlayer(moveRow, moveCol, player);
 		} else {
-			System.out.println("That was a bad input");
+			System.out.println("That was a bad input, try again.");
+			g.currentState.printState();
+			runGame(g);
 		}
 	}
 	public static void AIMove(Game g) {
-		g.updateBoardAI();
+		if(g.boardWidth == 4) {
+			g.updateBoardAI();
+		} else if(g.boardWidth == 8) {
+			g.updateBoardAIAB();
+		}
 	}
-	
-	//PlayerMove(State currentBoard, String nextMove)
-	// checks validity of move
-	// returns new board state
-	//AIMove(State currentBoard)
-	// obtains new board state from calling minimax
-	// returns new board state
 	
 	
 	public static int letterToNumber(char col) {
